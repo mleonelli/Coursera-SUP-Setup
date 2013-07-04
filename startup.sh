0d3868daffab6ac99675b291ce18e71a6549f899
Tomasini Marcello <whitehatty@gmail.com>
2013-07-03 23:11:47 -0400

    now calls setup.sh of lecture 4b

--- startup.sh
+++ startup.sh
@@ -6,38 +6,12 @@ sudo apt-get update 1> /dev/null
 echo "(INFO) ### Running apt-get upgrade ###"
 sudo apt-get -y upgrade 1> /dev/null
 
-# install git
-"(INFO) ### Installing git... ###"
-sudo apt-get install -y git-core
-if [ $? -eq 0 ] 
-then 
-	echo "(INFO) ### Git installed correctly ###"
-else
-	echo "(EE) ===>  GIT FAILED <==="
-fi
-
 # install heroku
 echo "(INFO) ### Installing heroku... ###"
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh 1> /dev/null
-if [ $? -eq 0 ] 
-then 
-	echo "(INFO) ### Heroku installed correctly ###"
-else
-        echo "(EE) ===> HEROKU FAILED <==="
-fi
 
-# install node.js
-echo "(INFO) ### Adding node.js ppa ###"
-sudo add-apt-repository  -y ppa:chris-lea/node.js
-sudo apt-get update 1> /dev/null
-echo "(INFO) ### installing node.js... ###"
-sudo apt-get install -y nodejs 1> /dev/null
-if [ $? -eq 0 ]
-then 
-	echo "(INFO) ### Node.js installed correctly ###"
-else
-        echo "(EE) ===> NODE.JS FAILED <==="
-fi
+echo "(INFO) ### running https://raw.github.com/startup-class/setup/master/setup.sh... ###"
+wget -qO- https://raw.github.com/startup-class/setup/master/setup.sh | sh 1> /dev/null
 
 # install some npm modules
 echo "(INFO) ### adding node modules... ###"
@@ -46,7 +20,9 @@ npm install restler csv accounting 1> /dev/null
 echo "git have been installed in `which git`"
 echo "heroku have been installed in `which heroku`"
 echo "node.js have been installed in `which node`"
+echo "nvm have been installed in `which nvm`"
 echo "npm have been installed in `which npm`"
+echo "emacs have been installed in `which emacs`"
 
 # login to heroku
 heroku login
